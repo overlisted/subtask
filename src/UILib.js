b64d4effc49a083f2464ab9af9eb61f17305163e
Ilya Maximov <overlisted@protonmail.ch>
2020-07-08 19:37:27 +0200

    new init from scratch

---
+++ src/UILib.js
@@ -0,0 +1,2 @@
+import React from "react";
+import {observer} from "mobx-react"
---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+import {} from './UILib'
+import {observer} from "mobx-react"
+
+class App extends React.Component {
+  render() {
+    return (
+      <></>
+    )
+  }
+}
+
+export default App;
