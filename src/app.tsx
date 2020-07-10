f42acc81b47cd0424fa2b29b1cdb9922a5498cc1
Ilya Maximov <overlisted@protonmail.ch>
2020-07-10 20:51:05 +0200

    add groups hierarchy builder

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "./app.css";
 import {observer} from "mobx-react";
-import {GroupTask} from "./task";
-import {observable} from "mobx";
-import {TaskDetails} from "./taskDetails";
+import {GroupTask, Task} from "./task";
+import {computed, observable} from "mobx";
+import {TaskDetails, TaskListColumns} from "./taskDetails";
 
 @observer
 class App extends React.Component {
   @observable mainGroup: GroupTask = new GroupTask("\0global", [], null);
+
+  @computed get groupsHierarchy() {
+    const result: GroupTask[] = [this.mainGroup];
+    let selected: Task<any> | null = this.mainGroup;
+
+    while(selected instanceof GroupTask) {
+      selected = selected.selected;
+      if(selected instanceof GroupTask) result.push(selected);
+    }
+
+    return result;
+  }
 
   render() {
     return (
