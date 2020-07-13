56dde9eeb9166f83dc5015c493dba61df5318086
Ilya Maximov <overlisted@protonmail.ch>
2020-07-13 21:18:40 +0200

    make task description editable

--- src/stringTaskView.tsx
+++ src/stringTaskView.tsx
@@ -1,17 +1,31 @@
 import React from "react";
 import {Task} from "./task";
 import {observer} from "mobx-react";
+import {observable} from "mobx";
+import {Field} from "./UILib/editable";
 
 @observer
 export default class StringTaskView extends React.Component<{task: Task<string>}> {
+  @observable isEditingContent = false;
+
   render() {
     const {task} = this.props;
 
     return (
       <>
-        <span>
-          {task.content}
-        </span>
+        <div>
+          {
+            this.isEditingContent
+              ? <Field value={task.content} setValue={name => task.content = name}/>
+              : <span>{task.content}</span>
+          }
+          <span
+            onClick={() => this.isEditingContent = !this.isEditingContent}
+            className="clickable not-important"
+          >
+            {this.isEditingContent ? "Escape" : "Edit"}
+          </span>
+        </div>
         <span className="not-important">
           Created at: {task.dateCreated.toDateString()}
         </span>
